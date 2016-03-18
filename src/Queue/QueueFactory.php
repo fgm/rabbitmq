aad8fc3ef40217e99bafc5e3a3d81476cb09b377
Carlos Jimenez <Saphyel@gmail.com>
2016-03-18 12:41:23 +0000

    Update QueueFactory.php

--- src/Queue/QueueFactory.php
+++ src/Queue/QueueFactory.php
@@ -3,12 +3,6 @@
 /**
  * @file
  * Contains the RabbitMQ QueueFactory.
- *
- * @author: Frédéric G. MARAND <fgm@osinet.fr>
- *
- * @copyright (c) 2015 Ouest Systèmes Informatiques (OSInet).
- *
- * @license General Public License version 2 or later
  */
 
 namespace Drupal\rabbitmq\Queue;
