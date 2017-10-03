5a4fb8b5edb6cbf4ee60ceacf3f071da3f2b08e7
Frederic G. MARAND <fgm@osinet.fr>
2017-10-03 18:59:34 +0200

    Reorder to match parent commit and minimize changeset.

--- src/Consumer.php
+++ src/Consumer.php
@@ -30,15 +30,12 @@ use PhpAmqpLib\Message\AMQPMessage;
  * to support multiple ways of accessing options, e.g. Drush vs Console vs Web.
  */
 class Consumer {
-
   use StringTranslationTrait;
 
   const EXTENSION_PCNTL = 'pcntl';
 
   const OPTION_MAX_ITERATIONS = 'max_iterations';
-
   const OPTION_MEMORY_LIMIT = 'memory_limit';
-
   const OPTION_TIMEOUT = 'rabbitmq_timeout';
 
   // Known option names and their default value.
@@ -113,26 +110,6 @@ class Consumer {
   }
 
   /**
-   * Implements hook_requirements().
-   */
-  public static function hookRequirements($phase, array &$req) {
-    $key = QueueBase::MODULE . '-consumer';
-    $req[$key]['title'] = t('RabbitMQ Consumer');
-    $options = [
-      ':ext' => Url::fromUri('http://php.net/pcntl')->toString(),
-      '%option' => static::OPTION_TIMEOUT,
-    ];
-    if (!extension_loaded(static::EXTENSION_PCNTL)) {
-      $req[$key]['description'] = t('Extension <a href=":ext">PCNTL</a> not present in PHP. Option  %option is not available in the RabbitMQ consumer.', $options);
-      $req[$key]['severity'] = REQUIREMENT_WARNING;
-    }
-    else {
-      $req[$key]['description'] = t('Extension <a href=":ext">PCNTL</a> is present in PHP. Option   %option is available in the RabbitMQ consumer.', $options);
-      $req[$key]['severity'] = REQUIREMENT_OK;
-    }
-  }
-
-  /**
    * Is the queue name valid ?
    *
    * @param string $queueName
@@ -151,42 +128,23 @@ class Consumer {
   }
 
   /**
-   * Log an event about the queue run.
-   */
-  public function logStart() {
-    $this->preFlightCheck();
-    $maxIterations = $this->getOption(self::OPTION_MAX_ITERATIONS);
-    if ($maxIterations > 0) {
-      $readyMessage = "RabbitMQ worker ready to receive up to @count messages.";
-      $readyArgs = ['@count' => $maxIterations];
-    }
-    else {
-      $readyMessage = "RabbitMQ worker ready to receive an unlimited number of messages.";
-      $readyArgs = [];
-    }
-    $this->logger->debug($readyMessage, $readyArgs, WATCHDOG_INFO);
-  }
-
-  /**
-   * Ensures options are consistent with configuration.
+   * Decode the data received from the queue using a chain of decoder choices.
    *
-   * @throws \Drupal\rabbitmq\Exception\InvalidArgumentException
-   *   Options are not compatible with configuration.
+   * - 1st/2nd choices: the one already set on the service instance
+   *   - 1st: set on the service instance manually during or after construction.
+   *   - 2nd: the one set on the service instance within consume() if the
+   *     worker implements DecoderAwareInterface.
+   * - 3rd choice: a legacy-compatible JSON decoder.
+   *
+   * @param mixed $data
+   *   The message payload to decode.
+   *
+   * @return mixed
+   *   The decoded value.
    */
-  protected function preFlightCheck() {
-    if ($this->pfcOk) {
-      return;
-    }
-    $this->pfcOk = FALSE;
-    $timeout = $this->getOption(self::OPTION_TIMEOUT);
-    if (!empty($timeout) && !extension_loaded(static::EXTENSION_PCNTL)) {
-      $message = $this->t('Option @option is not available without the @ext extension.', [
-        '@option' => static::OPTION_TIMEOUT,
-        '@ext' => static::EXTENSION_PCNTL,
-      ]);
-      throw new InvalidArgumentException($message);
-    }
-    $this->pfcOk = TRUE;
+  public function decode($data) {
+    $decoder = $this->decoder ?? 'json_decode';
+    return $decoder($data);
   }
 
   /**
@@ -208,6 +166,23 @@ class Consumer {
   }
 
   /**
+   * Log an event about the queue run.
+   */
+  public function logStart() {
+    $this->preFlightCheck();
+    $maxIterations = $this->getOption(self::OPTION_MAX_ITERATIONS);
+    if ($maxIterations > 0) {
+      $readyMessage = "RabbitMQ worker ready to receive up to @count messages.";
+      $readyArgs = ['@count' => $maxIterations];
+    }
+    else {
+      $readyMessage = "RabbitMQ worker ready to receive an unlimited number of messages.";
+      $readyArgs = [];
+    }
+    $this->logger->debug($readyMessage, $readyArgs, WATCHDOG_INFO);
+  }
+
+  /**
    * Signal handler.
    *
    * @see \Drupal\rabbitmq\Consumer::consume()
@@ -219,13 +194,6 @@ class Consumer {
     drupal_set_message($this->t('Timeout reached'));
     $this->logger->info('Timeout reached');
     $this->stopListening();
-  }
-
-  /**
-   * Mark listening as inactive.
-   */
-  public function stopListening() {
-    $this->continueListening = FALSE;
   }
 
   /**
@@ -308,40 +276,72 @@ class Consumer {
   }
 
   /**
-   * Mark listening as active.
-   */
-  public function startListening() {
-    $this->continueListening = TRUE;
-  }
-
-  /**
-   * Get a worker instance for a queue name.
+   * Provide a message callback for events.
    *
+   * @param \Drupal\Core\Queue\QueueWorkerInterface $worker
+   *   The worker plugin.
    * @param string $queueName
-   *   The name of the queue for which to get a worker.
+   *   The queue name.
+   * @param int $timeout
+   *   The queue wait timeout. Since it is only for queue wait, not worker wait,
+   *   it has to be reset before starting work, and reinitialized when ending
+   *   work.
    *
-   * @return \Drupal\Core\Queue\QueueWorkerInterface
-   *   The worker instance.
-   *
-   * @throws \Drupal\rabbitmq\Exception\InvalidWorkerException
+   * @return \Closure
+   *   The callback.
    */
-  protected function getWorker(string $queueName): QueueWorkerInterface {
-    // Before we start listening for messages, make sure the worker is valid.
-    $worker = $this->workerManager->createInstance($queueName);
-    if (!($worker instanceof QueueWorkerInterface)) {
-      throw new InvalidWorkerException("Invalid worker for requested queue.");
-    }
-    return $worker;
-  }
+  protected function getCallback(
+    QueueWorkerInterface $worker,
+    string $queueName,
+    int $timeout = 0
+  ): \Closure {
+    $callback = function (AMQPMessage $msg) use ($worker, $queueName, $timeout) {
+      if ($timeout) {
+        pcntl_alarm(0);
+      }
+      $this->logger->info('(Drush) Received queued message: @id', [
+        '@id' => $msg->delivery_info['delivery_tag'],
+      ]);
+      /** @var \Drupal\rabbitmq\Queue\Queue $queue */
+      $queue = $this->queueFactory->get($queueName);
+      try {
+        $id = $msg->delivery_info['delivery_tag'];
+        // Build the item to pass to the queue worker.
+        $item = (object) [
+          'id' => $msg->delivery_info['delivery_tag'],
+          'data' => $this->decode($msg->body),
+        ];
 
-  /**
-   * Register a decoder for message payloads.
-   *
-   * @param callable $decoder
-   *   The decoder.
-   */
-  public function setDecoder(callable $decoder) {
-    $this->decoder = $decoder;
+        $queue->addMessage($msg->delivery_info['delivery_tag'], $msg);
+
+        // Call the queue worker.
+        $worker->processItem($item->data);
+
+        // Remove the item from the queue.
+        $msg->delivery_info['channel']->basic_ack($item->id);
+        $this->logger->info('(Drush) Item @id acknowledged from @queue', [
+          '@id' => $item->id,
+          '@queue' => $queueName,
+        ]);
+      }
+      catch (RequeueException $e) {
+        $queue->releaseItem($item);
+      }
+      catch (SuspendQueueException $e) {
+        watchdog_exception('rabbitmq', $e);
+        $queue->releaseItem($item);
+      }
+      catch (\Exception $e) {
+        watchdog_exception('rabbitmq', $e);
+        $msg->delivery_info['channel']->basic_reject($msg->delivery_info['delivery_tag'],
+          TRUE);
+      }
+      if ($timeout) {
+        pcntl_alarm($timeout);
+      }
+    };
+
+    return $callback;
   }
 
   /**
@@ -381,74 +381,46 @@ class Consumer {
   }
 
   /**
-   * Provide a message callback for events.
+   * Get a worker instance for a queue name.
    *
-   * @param \Drupal\Core\Queue\QueueWorkerInterface $worker
-   *   The worker plugin.
    * @param string $queueName
-   *   The queue name.
-   * @param int $timeout
-   *   The queue wait timeout. Since it is only for queue wait, not worker wait,
-   *   it has to be reset before starting work, and reinitialized when ending
-   *   work.
+   *   The name of the queue for which to get a worker.
    *
-   * @return \Closure
-   *   The callback.
+   * @return \Drupal\Core\Queue\QueueWorkerInterface
+   *   The worker instance.
+   *
+   * @throws \Drupal\rabbitmq\Exception\InvalidWorkerException
    */
-  protected function getCallback(
-    QueueWorkerInterface $worker,
-    string $queueName,
-    int $timeout = 0
-  ): \Closure {
-    $callback = function (AMQPMessage $msg) use ($worker, $queueName, $timeout) {
-      if ($timeout) {
-        pcntl_alarm(0);
-      }
-      $this->logger->info('(Drush) Received queued message: @id', [
-        '@id' => $msg->delivery_info['delivery_tag'],
-      ]);
-      /** @var \Drupal\rabbitmq\Queue\Queue $queue */
-      $queue = $this->queueFactory->get($queueName);
-      try {
+  protected function getWorker(string $queueName): QueueWorkerInterface {
+    // Before we start listening for messages, make sure the worker is valid.
+    $worker = $this->workerManager->createInstance($queueName);
+    if (!($worker instanceof QueueWorkerInterface)) {
+      throw new InvalidWorkerException("Invalid worker for requested queue.");
+    }
+    return $worker;
+  }
 
-        $id = $msg->delivery_info['delivery_tag'];
-        // Build the item to pass to the queue worker.
-        $item = (object) [
-          'id' => $msg->delivery_info['delivery_tag'],
-          'data' => $this->decode($msg->body),
-        ];
-
-        $queue->addMessage($msg->delivery_info['delivery_tag'], $msg);
-
-        // Call the queue worker.
-        $worker->processItem($item->data);
-
-        // Remove the item from the queue.
-        $msg->delivery_info['channel']->basic_ack($item->id);
-        $this->logger->info('(Drush) Item @id acknowledged from @queue', [
-          '@id' => $item->id,
-          '@queue' => $queueName,
+  /**
+   * Did consume() hit the max_iterations limit ?
+   *
+   * @param int $maxIterations
+   *   The value of the max_iterations option.
+   * @param int $iteration
+   *   The current number of iterations in the consume() loop.
+   *
+   * @return bool
+   *   Did it ?
+   */
+  protected function hitIterationsLimit(int $maxIterations, int $iteration) {
+    if ($maxIterations > 0 && $maxIterations <= $iteration) {
+      $this->logger->notice('RabbitMQ worker has reached max number of iterations: @count. Exiting.',
+        [
+          '@count' => $maxIterations,
         ]);
-      }
-      catch (RequeueException $e) {
-        $queue->releaseItem($item);
-      }
-      catch (SuspendQueueException $e) {
-        watchdog_exception('rabbitmq', $e);
-        $queue->releaseItem($item);
-      }
-      catch (\Exception $e) {
-        watchdog_exception('rabbitmq', $e);
-        $msg->delivery_info['channel']->basic_reject($msg->delivery_info['delivery_tag'],
-          TRUE);
-      }
+      return TRUE;
+    }
 
-      if ($timeout) {
-        pcntl_alarm($timeout);
-      }
-    };
-
-    return $callback;
+    return FALSE;
   }
 
   /**
@@ -482,46 +454,45 @@ class Consumer {
   }
 
   /**
-   * Did consume() hit the max_iterations limit ?
-   *
-   * @param int $maxIterations
-   *   The value of the max_iterations option.
-   * @param int $iteration
-   *   The current number of iterations in the consume() loop.
-   *
-   * @return bool
-   *   Did it ?
+   * Implements hook_requirements().
    */
-  protected function hitIterationsLimit(int $maxIterations, int $iteration) {
-    if ($maxIterations > 0 && $maxIterations <= $iteration) {
-      $this->logger->notice('RabbitMQ worker has reached max number of iterations: @count. Exiting.',
-        [
-          '@count' => $maxIterations,
-        ]);
-      return TRUE;
+  public static function hookRequirements($phase, array &$req) {
+    $key = QueueBase::MODULE . '-consumer';
+    $req[$key]['title'] = t('RabbitMQ Consumer');
+    $options = [
+      ':ext' => Url::fromUri('http://php.net/pcntl')->toString(),
+      '%option' => static::OPTION_TIMEOUT,
+    ];
+    if (!extension_loaded(static::EXTENSION_PCNTL)) {
+      $req[$key]['description'] = t('Extension <a href=":ext">PCNTL</a> not present in PHP. Option  %option is not available in the RabbitMQ consumer.', $options);
+      $req[$key]['severity'] = REQUIREMENT_WARNING;
     }
-
-    return FALSE;
+    else {
+      $req[$key]['description'] = t('Extension <a href=":ext">PCNTL</a> is present in PHP. Option   %option is available in the RabbitMQ consumer.', $options);
+      $req[$key]['severity'] = REQUIREMENT_OK;
+    }
   }
 
   /**
-   * Decode the data received from the queue using a chain of decoder choices.
+   * Ensures options are consistent with configuration.
    *
-   * - 1st/2nd choices: the one already set on the service instance
-   *   - 1st: set on the service instance manually during or after construction.
-   *   - 2nd: the one set on the service instance within consume() if the
-   *     worker implements DecoderAwareInterface.
-   * - 3rd choice: a legacy-compatible JSON decoder.
-   *
-   * @param mixed $data
-   *   The message payload to decode.
-   *
-   * @return mixed
-   *   The decoded value.
+   * @throws \Drupal\rabbitmq\Exception\InvalidArgumentException
+   *   Options are not compatible with configuration.
    */
-  public function decode($data) {
-    $decoder = $this->decoder ?? 'json_decode';
-    return $decoder($data);
+  protected function preFlightCheck() {
+    if ($this->pfcOk) {
+      return;
+    }
+    $this->pfcOk = FALSE;
+    $timeout = $this->getOption(self::OPTION_TIMEOUT);
+    if (!empty($timeout) && !extension_loaded(static::EXTENSION_PCNTL)) {
+      $message = $this->t('Option @option is not available without the @ext extension.', [
+        '@option' => static::OPTION_TIMEOUT,
+        '@ext' => static::EXTENSION_PCNTL,
+      ]);
+      throw new InvalidArgumentException($message);
+    }
+    $this->pfcOk = TRUE;
   }
 
   /**
@@ -539,6 +510,16 @@ class Consumer {
   }
 
   /**
+   * Register a decoder for message payloads.
+   *
+   * @param callable $decoder
+   *   The decoder.
+   */
+  public function setDecoder(callable $decoder) {
+    $this->decoder = $decoder;
+  }
+
+  /**
    * Register a method able to get option values.
    *
    * @param callable $optionGetter
@@ -546,6 +527,20 @@ class Consumer {
    */
   public function setOptionGetter(callable $optionGetter) {
     $this->optionGetter = $optionGetter;
+  }
+
+  /**
+   * Mark listening as active.
+   */
+  public function startListening() {
+    $this->continueListening = TRUE;
+  }
+
+  /**
+   * Mark listening as inactive.
+   */
+  public function stopListening() {
+    $this->continueListening = FALSE;
   }
 
 }
